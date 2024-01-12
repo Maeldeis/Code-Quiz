6829996f7e93fe3cd35be5a08cc77e79327adf1a
Maeldeis <maeldeis@gmail.com>
2024-01-12 12:54:23 +0100

    if loop for all scores into list

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,20 +1,23 @@
-// Declared variables
+//variables
 var highScore = document.querySelector (#highScore");
 var clear = document.querySelector (#clear");
 var goBack = document.querySelector(#goBack);  
  
-// Event listener to clear scores 
+//event listener clear
+clear.addEventListener("click", function ()) {
+    localStorage.clear();
+    location.reload();
+});
 
-// Retreives local stroage 
-
-if (allScores !== null) {
-
-    for (var i = 0; i < allScores.length; i++) {
-
+// inputs local stroage if loop
+var allScores = localStorage.getItem("allScores")
+allScores=localStorage.getItem("allScores");
+allScores=JSON.parse(allScores)
+if (allScores !==null) {
+    for (let i = 0; i < allScores.length; i++) {
         var createLi = document.createElement("li");
-        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        createLi.textContent = allScores[i].initials + " "+allScores[i].score;
         highScore.appendChild(createLi);
-
-    }
-}
-// Event listener to move to index page
+         }
+        }
+// event listener move to index page
