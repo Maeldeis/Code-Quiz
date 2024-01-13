d502340db609772e96b4bd61dc25e426c7a8991b
Maeldeis <maeldeis@gmail.com>
2024-01-13 22:21:44 +0100

    moved separate js files into one

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,21 +15,33 @@ var questions = [
         choices:["x","y"],
         answer:"x"
     },
+]
+
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+//variables
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");  
  
+//event listener clear
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    highScore.innerHTML= "";
+});
 
-];
-console.log(questions[2].question);
-// loop through an array of the choices for each question
-
-
-var 
-var startEl = document.getElementById("start")
-//this function should include node to make button disappear
-
-function startQuiz(){
-    console.log("hello");
-}
-startEl.addEventListener("click", startQuiz);
-// var, what is, on what, call back to function
-
-
+// inputs local stroage if loop
+var allScores = localStorage.getItem("allScores");
+allScores=localStorage.getItem("allScores");
+allScores=JSON.parse(allScores)
+if (allScores !==null) {
+    for (let i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " "+allScores[i].score;
+        highScore.appendChild(createLi);
+         }
+        }
+// event listener move to index page
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
