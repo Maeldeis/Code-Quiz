ba8582974c3f317d2c04c3c828c7219e29b40cd0
Maeldeis <maeldeis@gmail.com>
2024-01-13 22:27:03 +0100

    variables for timer funcs added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Array var object
 
 var questions = [
     {
@@ -8,7 +9,7 @@ var questions = [
     {
         question:"what2",
         choices:["x","y"],
-        answer:"x"
+        answer:"y"
     },
     {
         question:"what3",
@@ -17,31 +18,8 @@ var questions = [
     },
 ]
 
+var score = 0;
+var questionI = 0;
 var currentTime = document.querySelector("#currentTime");
 var timer = document.querySelector("#startTime");
 var questionsDiv = document.querySelector("#questionsDiv");
-//variables
-var highScore = document.querySelector("#highScore");
-var clear = document.querySelector("#clear");
-var goBack = document.querySelector("#goBack");  
- 
-//event listener clear
-clear.addEventListener("click", function () {
-    localStorage.clear();
-    highScore.innerHTML= "";
-});
-
-// inputs local stroage if loop
-var allScores = localStorage.getItem("allScores");
-allScores=localStorage.getItem("allScores");
-allScores=JSON.parse(allScores)
-if (allScores !==null) {
-    for (let i = 0; i < allScores.length; i++) {
-        var createLi = document.createElement("li");
-        createLi.textContent = allScores[i].initials + " "+allScores[i].score;
-        highScore.appendChild(createLi);
-         }
-        }
-// event listener move to index page
-goBack.addEventListener("click", function () {
-    window.location.replace("./index.html");
